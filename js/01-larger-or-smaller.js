c20aff6a66448227701453313e2da02334de12d7
Alan Chen <alrichen@ucsc.edu>
2025-02-21 03:11:58 -0800

    Added files and started 1st part

---
+++ js/01-larger-or-smaller.js
@@ -0,0 +1,39 @@
+console.log("Is 10.000001 an integer? " + Number.isInteger(10.000001));
+console.log("Is 10.000000 an integer? " + Number.isInteger(10.0));
+console.log("Is 10.000001 equal to 10.000000? " + (10.000001 === 10.0));
+console.log("Does 10.000001 become 10 when converted to an integer? " + parseInt(10.000001));
+
+document.write("Comparing Two Integers:<br>");
+let a = prompt("Enter the first integer:");
+let b = prompt("Enter the second integer:");
+
+ 
+if ((isNaN(a) || isNaN(b))) {
+    document.write("Please enter valid integers.<br>");
+}
+// According to JavaScript, 5.0 is an integer
+else if(Number.isInteger(parseFloat(a)) === false || Number.isInteger(parseFloat(b)) === false) {
+    document.write("Please enter valid integers.<br>");
+}
+// 10.000001 will become 10 when converted to an integer.
+// Don't allow 10.0000001 to be considered an integer.
+// else if (parseFloat(a) % 1 !== 0 || parseFloat(b) % 1 !== 0) {
+//     document.write("Please enter valid integers.<br>");
+// }
+else if (Math.floor(parseFloat(a)) !== parseFloat(a) || Math.floor(parseFloat(b)) !== parseFloat(b)) {
+    document.write("Please enter valid integers.<br>");
+}
+// Check for decimal point in input
+// Is 5.0 considered an integer?
+// else if (a.includes(".") || b.includes(".")) {
+//     document.write("Please enter valid integers.<br>");
+// }
+else if (parseInt(a) > parseInt(b)) {
+    document.write(`${a} is the largest integer that was entered.<br>`);
+}
+else if (parseInt(a) < parseInt(b)) {
+    document.write(`${b} is the largest integer that was entered.<br>`);
+}
+else {
+    document.write(`The two integers you have input are equal.<br>`);
+}
